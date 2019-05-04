9e5bed29ae287f97f276c7b12e0a11892307e55d
Aruna Benjamin <arunabenji29@gmail.com>
2019-05-04 17:43:10 -0600

    stretch carousel working

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -2,32 +2,65 @@ import React, { Component } from 'react';
 import { carouselData } from '../../data'
 // Complete this Carousel 
 export default class Carousel extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-
+      carouselImages: [],
+      currentIndex:0,
     }
   }
-  componentDidMount(){
-
+  componentDidMount() {
+    this.setState({
+      carouselImages: carouselData
+    })
   }
 
-  leftClick = () => {
+  leftClick = (event) => {
+    event.preventDefault();
+    if(this.state.currentIndex <1){
+      this.setState({
+        currentIndex:this.state.carouselImages.length-1
+      })
+    }
+      else{
+        this.setState(prevState => ({
+          currentIndex: prevState.currentIndex - 1
+        })
+        );
+      }
+    
 
+    
+    console.log('left click current INdex '+this.state.currentIndex)
+    this.selectedImage()
   }
 
-  rightClick = () => {
-
+  rightClick = (event) => {
+    event.preventDefault();
+    if(this.state.currentIndex >= this.state.carouselImages.length-1){
+      this.setState({
+        currentIndex:0
+      })
+    }
+    else{
+    this.setState({
+      currentIndex: this.state.currentIndex + 1
+    })
+    
+  }
+    console.log('right click current INdex '+this.state.currentIndex)
+    
   }
 
-  selectedImage = () => {
-    return <img src={} style={{display: 'block'}} />
+  selectedImage = (curIndex) => {
+    return <img src={this.state.carouselImages[curIndex]} style={{ display: 'flex' }} />
   }
-  
-  render(){
+
+  render() {
     return (
       <div className="carousel">
         <div className="left-button" onClick={this.leftClick}>{"<"}</div>
+        {this.selectedImage(this.state.currentIndex)}
         <div className="right-button" onClick={this.rightClick}>{">"}</div>
       </div>
     )
--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Tabs from './Tabs';
 import Cards from './Cards';
+import Carousel from '../Carousel/Carousel'
 
 // Importing our tab and card data. No need to change anything here.
 import { tabData, cardData } from '../../data';
@@ -70,6 +71,7 @@ export default class Content extends Component {
         */}
         <button className='logout' onClick={this.logoutSubmit}>Logout 👈</button>
         <Tabs tabs={this.state.tabs} selectedTab={this.state.selected} selectTabHandler={this.changeSelected} />
+        <Carousel />
         <Cards cards={this.filterCards()} />
       </div>
     );
